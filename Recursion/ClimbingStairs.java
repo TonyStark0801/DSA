9c6d92e93a81aea2bafa950382633cd352a00f88
Shubham Mishra <mishrashubham2001@gmail.com>
2022-08-02 01:32:42 +0530

    Climbing stair problem

---
+++ Recursion/ClimbingStairs.java
@@ -0,0 +1,29 @@
+/*Working fine with recursion but
+ * !TIME LIMIT EXCEEDED
+ * for N = 45 >> 1836311903
+ * so we will use DP .
+ * https://leetcode.com/problems/climbing-stairs/submissions/
+*/
+
+
+public class ClimbingStairs {
+    public static void main(String args[]){
+        int N = 45;
+        System.out.println(steps(N));
+    }
+
+    static int steps(int n){
+        
+        if(n<0){
+            return 0;
+        }
+        
+        if(n==0){
+            return 1;
+        }
+        return steps(n-1)+steps(n-2);
+        
+        
+        
+    }
+}
