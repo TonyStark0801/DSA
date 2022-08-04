be44b64406c8b52826785f41085b5df49919103e
SHUBHAM MISHRA <60005297+TonyStark0801@users.noreply.github.com>
2022-08-04 13:08:33 +0530

    Create leetcode.java

---
+++ Recursion/leetcode.java
@@ -0,0 +1,16 @@
+public class test{
+    public static void main(String args[]){
+       int n =-123;
+       int ans =0;
+       while(n!=0){
+        int digit = n%10;
+        System.out.println(digit);
+        ans = ans * 10 + digit;
+        n=n/10;
+       }
+       if(ans<=)
+       System.out.println(ans);
+
+    }
+
+}
